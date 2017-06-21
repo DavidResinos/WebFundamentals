132c19b47863d113474d67b8c2b19cb88f01da9a
David <resinosdavid@gmail.com>
2017-06-21 16:17:24 -0400

    New Folder

---
+++ JavaScript_Basics/0.01_intergers.js
@@ -0,0 +1,21 @@
+var tired = false;
+var hungry = false;
+var old = false;
+var old = false;
+
+
+2>1;
+3 < 2;
+5>= 10;
+console.log(2===2)
+console.log(2+2)
+
+
+console.log("Paul" !== "Julie");
+console.log("Letmein1234!" !== "Password1!");
+
+
+//Logical Operators 
+
+// && and
+// || or 
---
+++ JavaScript_Basics/0.05_conditionals.js
@@ -0,0 +1,24 @@
+
+//if & else
+
+
+var age = 21;
+
+if (age === 40){
+	console.log("Man, you are so cool!");
+} else if (age > 25){
+	console.log("Cool you can rent a car!")
+} else if (age>= 18) {
+	console.log("Congrats, you can vote.")
+}
+var password = "Letmein1231"
+
+if (password === "Letmein1231"){
+	console.log("Welcome User!")
+}else {
+	console.log("Who are You?")
+}
+
+
+
+
